50310531f9f83a626a4cdfb876332ba9ec2b1d87
yuanqing <hello@yuanqing.sg>
2014-12-28 10:52:53 +0800

    Add tests for Object comparisons

--- test/versus.spec.js
+++ test/versus.spec.js
@@ -11,24 +11,36 @@ describe('versus(a, op, b)', function() {
       expect(versus(1, '==', 1)).toBe(true);
       expect(versus(1, '==', 2)).toBe(false);
       expect(versus(2, '==', 1)).toBe(false);
+      expect({ foo: 1 } == { foo: 1 }).toBe(false);
+      expect(versus({ foo: 1 }, '==', { foo: 1 })).toBe(true);
+      expect(versus({ foo: 1 }, '==', { foo: 2 })).toBe(false);
     });
 
     it('a === b', function() {
       expect(versus(1, '===', 1)).toBe(true);
       expect(versus(1, '===', 2)).toBe(false);
       expect(versus(2, '===', 1)).toBe(false);
+      var a = { foo: 1 };
+      expect(versus(a, '===', { foo: 1 })).toBe(false);
+      expect(versus(a, '===', a)).toBe(true);
     });
 
     it('a != b', function() {
       expect(versus(1, '!=', 1)).toBe(false);
       expect(versus(1, '!=', 2)).toBe(true);
       expect(versus(2, '!=', 1)).toBe(true);
+      expect({ foo: 1 } != { foo: 1 }).toBe(true);
+      expect(versus({ foo: 1 }, '!=', { foo: 1 })).toBe(false);
+      expect(versus({ foo: 1 }, '!=', { foo: 2 })).toBe(true);
     });
 
     it('a !== b', function() {
       expect(versus(1, '!==', 1)).toBe(false);
       expect(versus(1, '!==', 2)).toBe(true);
       expect(versus(2, '!==', 1)).toBe(true);
+      var a = { foo: 1 };
+      expect(versus(a, '!==', { foo: 1 })).toBe(true);
+      expect(versus(a, '!==', a)).toBe(false);
     });
 
     it('a < b', function() {
